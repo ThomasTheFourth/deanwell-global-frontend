96f9562e1eb7058b84b873805f1f8a28e85c3f80
Thomas Barnwell <thomas@krakenlabs.io>
2023-03-17 16:24:39 -0400

    Shop update

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from "react";
 import ProductCard from "../../components/ProductCard";
-import { Container, LoaderImage, LoaderContainer, PageTitle } from "./styles";
+import {
+  Container,
+  LoaderImage,
+  LoaderContainer,
+  PageTitle,
+  GridContainer,
+  // FeaturedContainer,
+} from "./styles";
 import { backendUrl } from "../../constants/global";
 import loader from "./loader.gif";
-import FeaturedProductCard from "../../components/FeaturedProductCard";
+// import FeaturedProductCard from "../../components/FeaturedProductCard";
 
 const Shop = () => {
   const [data, setData] = useState([]);
-  console.log(data);
 
   useEffect(() => {
-    fetch(`${backendUrl}/api/products?populate=*`)
+    fetch(`${backendUrl}/api/products?populate=*&sort=title`)
       .then((res) => res.json())
       .then(
         (result) => {
