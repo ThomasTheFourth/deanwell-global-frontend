04f99708bccb0ff5c981197c4c9a811b44855350
Thomas Barnwell <thomas@krakenlabs.io>
2024-07-04 18:52:23 -0400

    Adding site message

---
+++ src/components/SiteMessage/index.js
@@ -0,0 +1,15 @@
+import { styled } from "@mui/system";
+
+export const SiteMessageContainer = styled("div")`
+    background-color: grey;
+    width: 100%;
+`;
+
+export const SiteMessage = styled("p")`
+    padding: 0 16px;
+    max-width: 1024px;
+    color: white;
+    margin: 0 auto;
+    text-align: center;
+    line-height: 150%;
+`;
